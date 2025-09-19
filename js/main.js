851d7ffffcfe02f4f75d54f60ff1c1a85d7619e2
David <david.20797@gmail.com>
2025-09-19 16:54:21 +0300

    html response handler

--- js/main.js
+++ js/main.js
@@ -61,7 +61,7 @@
     function validate (input) {
         var $i = $(input);
         if ($i.attr('type') === 'email' || $i.attr('name') === 'email') {
-            console.log('raw email:', $form.find('input[name="email"]').val());
+            // console.log('raw email:', $form.find('input[name="email"]').val());
             return /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/.test($i.val().trim());
         } else {
             return $i.val().trim() !== '';
